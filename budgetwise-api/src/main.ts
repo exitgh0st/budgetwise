1f0b0a18958f71e49c425a360901be57618a7d6d
exitgh0st <dev.exitgh0st@gmail.com>
2026-03-27 18:37:52 +0800

    Add ORIGIN environment variable to cors

--- budgetwise-api/src/main.ts
+++ budgetwise-api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: process.env.ORIGIN ?? 'http://localhost:4200',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
